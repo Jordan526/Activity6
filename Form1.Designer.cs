0b5587c4c70b00c337da77e4a95853e1a32bad83
Jordan526 <96961388+Jordan526@users.noreply.github.com>
2022-06-10 21:39:39 -0700

    Add files via upload
    Refined Activity 2 problem with exception handling and number formatting.

---
+++ Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace WeightApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.WelcomeTextbox = new System.Windows.Forms.TextBox();
+            this.SelfWeightBox = new System.Windows.Forms.TextBox();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.earthWeight = new System.Windows.Forms.Label();
+            this.marsWeight = new System.Windows.Forms.Label();
+            this.clearWeight = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // WelcomeTextbox
+            // 
+            this.WelcomeTextbox.Location = new System.Drawing.Point(15, 12);
+            this.WelcomeTextbox.Name = "WelcomeTextbox";
+            this.WelcomeTextbox.Size = new System.Drawing.Size(386, 20);
+            this.WelcomeTextbox.TabIndex = 0;
+            this.WelcomeTextbox.Text = "Check your weight on Mars!";
+            this.WelcomeTextbox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // SelfWeightBox
+            // 
+            this.SelfWeightBox.Location = new System.Drawing.Point(227, 84);
+            this.SelfWeightBox.Name = "SelfWeightBox";
+            this.SelfWeightBox.Size = new System.Drawing.Size(98, 20);
+            this.SelfWeightBox.TabIndex = 1;
+            this.SelfWeightBox.TextChanged += new System.EventHandler(this.SelfWeightBox_TextChanged);
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(181, 187);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(195, 23);
+            this.calculateButton.TabIndex = 3;
+            this.calculateButton.Text = "Click to Calculate Weight!";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // earthWeight
+            // 
+            this.earthWeight.AutoSize = true;
+            this.earthWeight.Location = new System.Drawing.Point(12, 91);
+            this.earthWeight.Name = "earthWeight";
+            this.earthWeight.Size = new System.Drawing.Size(179, 13);
+            this.earthWeight.TabIndex = 4;
+            this.earthWeight.Text = "Enter Your Weight (On Earth in LBS)";
+            // 
+            // marsWeight
+            // 
+            this.marsWeight.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.marsWeight.AutoSize = true;
+            this.marsWeight.Location = new System.Drawing.Point(12, 197);
+            this.marsWeight.Name = "marsWeight";
+            this.marsWeight.Size = new System.Drawing.Size(123, 13);
+            this.marsWeight.TabIndex = 5;
+            this.marsWeight.Text = "Your Weight On Mars Is:";
+            // 
+            // clearWeight
+            // 
+            this.clearWeight.Location = new System.Drawing.Point(227, 110);
+            this.clearWeight.Name = "clearWeight";
+            this.clearWeight.Size = new System.Drawing.Size(98, 23);
+            this.clearWeight.TabIndex = 6;
+            this.clearWeight.Text = "Clear Weight";
+            this.clearWeight.UseVisualStyleBackColor = true;
+            this.clearWeight.Click += new System.EventHandler(this.clearWeight_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 287);
+            this.Controls.Add(this.clearWeight);
+            this.Controls.Add(this.marsWeight);
+            this.Controls.Add(this.earthWeight);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.SelfWeightBox);
+            this.Controls.Add(this.WelcomeTextbox);
+            this.Name = "Form1";
+            this.Text = "  ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox WelcomeTextbox;
+        private System.Windows.Forms.TextBox SelfWeightBox;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Label earthWeight;
+        private System.Windows.Forms.Label marsWeight;
+        private System.Windows.Forms.Button clearWeight;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WeightApp
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        String weight;
+        private void SelfWeightBox_TextChanged(object sender, EventArgs e)
+        {
+
+
+            weight = SelfWeightBox.Text;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                 int userWeight = int.Parse(weight);
+                double weightOnMars = (double)(userWeight * 0.38);
+                double roundedNumber = (double)Math.Round(weightOnMars, 3);
+
+                MessageBox.Show("Your Weight On Mars Is " + roundedNumber.ToString("#.000") + " Pounds!");
+            }
+            catch (System.FormatException exception)
+            {
+                MessageBox.Show("Not a valid format, please try again.");
+            }
+        }
+
+        private void clearWeight_Click(object sender, EventArgs e)
+        {
+            SelfWeightBox.Text = "";
+        }
+    }
+}
+
+
+
